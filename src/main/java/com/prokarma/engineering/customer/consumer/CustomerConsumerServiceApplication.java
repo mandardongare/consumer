e94130e4ee6ddaef3dfaf48d0a01ec38181af37f
Mandar Dongare <mandar.dongare119@gmail.com>
2020-11-09 23:04:35 +0530

    application.properties changes

--- src/main/java/com/prokarma/engineering/customer/consumer/CustomerConsumerServiceApplication.java
+++ src/main/java/com/prokarma/engineering/customer/consumer/CustomerConsumerServiceApplication.java
@@ -2,12 +2,19 @@ package com.prokarma.engineering.customer.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CustomerConsumerServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CustomerConsumerServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(CustomerConsumerServiceApplication.class, args);
+  }
 
+  @GetMapping("/")
+  public String landingPage() {
+    return "<h2>Consumer Microservice v1.0</h2>";
+  }
 }
